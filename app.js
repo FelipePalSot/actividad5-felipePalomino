5d6c6fc7cbd65316b447a7b63701216aa8b06b85
Felipe Jean Franco Palomino Sotelo <felipejeanfrancopalominosotelo@MacBook-Pro-de-Felipe.local>
2025-11-10 16:46:26 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+import _express from "express";
+import _bodyParser from "body-parser";
+import _cors from "./config/cors.js";
+
+import PUERTO from "./utils/constantes.js";
+import api from "./routes.js"
+import { configSocket } from "./utils/socket.js";
+
+const app= _express();
+app.use(_bodyParser.json());
+app.use(_bodyParser.urlencoded({ extended: true, 
+    type: 'application/x-www-form-urlencoded' }));
+app.use(_cors);
+
+
+//... ruta raíz ...
+app.get("/", (req, res) => {
+    res.json({
+        mensaje: "API Backend - Proyecto Final - Felipe Palomino Sotelo",
+        version: "1.0",
+        endpoints: "/api/v1"
+    });
+});
+
+//... endpoints ...
+app.use("/api/v1", api);
+
+//... servidor ...
+const server = app.listen(PUERTO, () => {
+    console.log('Listening on '+PUERTO);
+});
+
+configSocket(server);
